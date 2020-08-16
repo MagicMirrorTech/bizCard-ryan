371e3a219afcf7ab258a1150ddd4d7c8715a3104
James Keck <mac@Macs-MacBook-Pro.local>
2020-08-15 20:23:12 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BcardComponent } from './bcard/bcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BcardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bcard/bcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bcard',
+  templateUrl: './bcard.component.html',
+  styleUrls: ['./bcard.component.css']
+})
+export class BcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
