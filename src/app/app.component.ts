9798a50cf6304b55a5c2b32c992744bc55353aff
James Keck <mac@Macs-MacBook-Pro.local>
2020-08-18 17:08:45 -0600

    removed import from router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
 
-import {
-  Router,
-  Event as RouterEvent,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError
-} from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
