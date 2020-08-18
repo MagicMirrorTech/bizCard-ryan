7454de7ed6d2ee69d29942a85085a927d5eff096
James Keck <mac@Macs-MacBook-Pro.local>
2020-08-18 16:21:13 -0600

    removed #

--- src/app/loading-page/loading-page.component.ts
+++ src/app/loading-page/loading-page.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadingPageComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+    }
+    
 }
